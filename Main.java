687157d564c7bfda550752690f00ce6c5507adca
Matthew Cairns <mattrcairns@gmail.com>
2014-06-03 19:48:42 +0100

    Setting up OpenGL

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.glu.GLU;
 
 public class Main {
     public Main(){
@@ -14,11 +15,7 @@ public class Main {
             e.printStackTrace();
         }
 
-        // init OpenGL
-        GL11.glMatrixMode(GL11.GL_PROJECTION);
-        GL11.glLoadIdentity();
-        GL11.glOrtho(0, 800, 0, 600, 1, -1);
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        InitGL();
 
         while(!Display.isCloseRequested()) {
             update();
@@ -32,17 +29,66 @@ public class Main {
     private void update() {
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
-        // set the color of the quad (R,G,B,A)
+        GL11.glLoadIdentity();
+
+        GL11.glTranslatef(0f,0.0f,-7f);
+        GL11.glRotatef(45f,0.0f,1.0f,0.0f);
         GL11.glColor3f(0.5f,0.5f,1.0f);
 
-        // draw quad
         GL11.glBegin(GL11.GL_QUADS);
-        GL11.glVertex2f(100,100);
-        GL11.glVertex2f(100+200,100);
-        GL11.glVertex2f(100+200,100+200);
-        GL11.glVertex2f(100,100+200);
+        GL11.glColor3f(1.0f,1.0f,0.0f);
+        GL11.glVertex3f( 1.0f, 1.0f,-1.0f);
+        GL11.glVertex3f(-1.0f, 1.0f,-1.0f);
+        GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
+        GL11.glVertex3f( 1.0f, 1.0f, 1.0f);
+        GL11.glColor3f(1.0f,0.5f,0.0f);
+        GL11.glVertex3f( 1.0f,-1.0f, 1.0f);
+        GL11.glVertex3f(-1.0f,-1.0f, 1.0f);
+        GL11.glVertex3f(-1.0f,-1.0f,-1.0f);
+        GL11.glVertex3f( 1.0f,-1.0f,-1.0f);
+        GL11.glColor3f(1.0f,0.0f,0.0f);
+        GL11.glVertex3f( 1.0f, 1.0f, 1.0f);
+        GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
+        GL11.glVertex3f(-1.0f,-1.0f, 1.0f);
+        GL11.glVertex3f( 1.0f,-1.0f, 1.0f);
+        GL11.glColor3f(1.0f,1.0f,0.0f);
+        GL11.glVertex3f( 1.0f,-1.0f,-1.0f);
+        GL11.glVertex3f(-1.0f,-1.0f,-1.0f);
+        GL11.glVertex3f(-1.0f, 1.0f,-1.0f);
+        GL11.glVertex3f( 1.0f, 1.0f,-1.0f);
+        GL11.glColor3f(0.0f,0.0f,1.0f);
+        GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
+        GL11.glVertex3f(-1.0f, 1.0f,-1.0f);
+        GL11.glVertex3f(-1.0f,-1.0f,-1.0f);
+        GL11.glVertex3f(-1.0f,-1.0f, 1.0f);
+        GL11.glColor3f(1.0f,0.0f,1.0f);
+        GL11.glVertex3f( 1.0f, 1.0f,-1.0f);
+        GL11.glVertex3f( 1.0f, 1.0f, 1.0f);
+        GL11.glVertex3f( 1.0f,-1.0f, 1.0f);
+        GL11.glVertex3f( 1.0f,-1.0f,-1.0f);
         GL11.glEnd();
 
+    }
+
+    private void InitGL() {
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glShadeModel(GL11.GL_SMOOTH);
+        GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        GL11.glClearDepth(1.0);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glDepthFunc(GL11.GL_LEQUAL);
+
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+
+        GLU.gluPerspective(
+                67.0f,
+                1280.0f/720.0f,
+                0.1f,
+                100.0f);
+
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        GL11.glHint(GL11.GL_PERSPECTIVE_CORRECTION_HINT, GL11.GL_NICEST);
     }
 
     public static void main(String[] args){

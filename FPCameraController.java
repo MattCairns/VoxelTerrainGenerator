ed130e9beb9c3eb8f79559ca4a86464cdff440d6
Matthew Cairns <mattrcairns@gmail.com>
2014-06-04 20:08:15 +0100

    Create a controller for first person camera

--- FPCameraController.java
+++ FPCameraController.java
@@ -1,5 +1,6 @@
 package com.matthewcairns.voxel;
 
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.vector.Vector3f;
 
 /**
@@ -13,4 +14,38 @@ public class FPCameraController {
     private float yaw = 0.0f;
     //Rotation around the X axis
     private float pitch = 0.0f;
+
+    public FPCameraController(float x, float y, float z) {
+        position = new Vector3f(x,y,z);
+    }
+
+    public void yaw(float value) { yaw += value; }
+    public void pitch(float value) { pitch += value; }
+
+    public void walkForward(float distance) {
+        position.x -= distance * (float)Math.sin(Math.toRadians(yaw));
+        position.y += distance * (float)Math.cos(Math.toRadians(yaw));
+    }
+
+    public void walkBackwards(float distance) {
+        position.x += distance * (float)Math.sin(Math.toRadians(yaw));
+        position.y -= distance * (float)Math.cos(Math.toRadians(yaw));
+    }
+
+    public void strafeLeft(float distance) {
+        position.x -= distance * (float)Math.sin(Math.toRadians(yaw-90));
+        position.y += distance * (float)Math.cos(Math.toRadians(yaw-90));
+    }
+
+    public void strafeRight(float distance) {
+        position.x -= distance * (float)Math.sin(Math.toRadians(yaw+90));
+        position.y += distance * (float)Math.cos(Math.toRadians(yaw+90));
+    }
+
+    public void lookThrough() {
+        GL11.glRotatef(pitch, 1.0f, 0.0f, 0.0f);
+        GL11.glRotatef(yaw, 0.0f, 1.0f, 0.0f);
+        GL11.glTranslatef(position.x, position.y, position.z);
+    }
+
 }

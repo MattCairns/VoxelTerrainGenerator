1ead8b6fbf42dde0d4698956e58de1296608685f
Matthew Cairns <mattrcairns@gmail.com>
2014-06-10 17:56:55 +0100

    Added HUD class for drawing text to screen.

---
+++ HUD.java
@@ -0,0 +1,25 @@
+package com.matthewcairns.voxel;
+
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.TrueTypeFont;
+
+import java.awt.Font;
+
+/**
+ * Created by Matthew Cairns on 10/06/2014.
+ * All rights reserved.
+ */
+public class HUD {
+
+    TrueTypeFont font;
+
+    public HUD(String typeFace, int fontSize) {
+        Font awtFont = new Font(typeFace, Font.BOLD, fontSize);
+        font = new TrueTypeFont(awtFont, false);
+    }
+
+    public void drawFont(int x, int y, String s) {
+        font.drawString(x,y,s, Color.white);
+    }
+}
